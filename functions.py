8b5548de543e9520e1961bf279d4ec3bc8e3028e
adilelle1 <adilelle20@ucema.edu.ar>
2022-06-06 23:44:36 -0300

    Segunda version TP

---
+++ functions.py
@@ -0,0 +1,150 @@
+def welcome():
+    print('\t\t*********************************')
+    print('\t\t* Welcome to your player finder *')
+    print('\t\t*********************************')
+
+
+def user_menu():
+    print('---------------------'
+          'Main menu:'
+          '\t(1) Find a player by its attributes and stats'
+          '\t(2) Find player stats by team'
+          '\t(3) Quit'
+          '---------------------')
+
+
+def team_finder():
+    import requests
+    team = input("\nInsert your team's name>>")
+
+    response = requests.get(
+        "https://api-nba-v1.p.rapidapi.com/teams?search=" + team,
+        headers={
+            'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+            'x-rapidapi-key': "1e5e5821femsh450b4f3086376a6p114414jsne8cc7f313f90"
+        }
+    )
+
+    status_code = response.status_code
+
+    if status_code == 200:
+        json = response.json()
+        # print(json['response']) -- permite ver todos los atributos
+        for team in json['response']:
+            return team['id']
+    else:
+        print('Error')
+
+
+def player_position_finder(player_id):
+    import requests
+    response = requests.get(
+        "https://api-nba-v1.p.rapidapi.com/player/statistics?season=2020&id=" + player_id,
+        headers={
+            'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+            'x-rapidapi-key': "1e5e5821femsh450b4f3086376a6p114414jsne8cc7f313f90"
+        }
+    )
+
+    status_code = response.status_code
+
+    if status_code == 200:
+        json = response.json()
+        # print(json['response']) -- permite ver todos los atributos
+        for player in json['response']:
+            return player['pos']
+    else:
+        print('Error')
+
+
+def player_stats(team, firstname, lastname):
+    import statistics
+    import requests
+
+    response = requests.get(
+        "https://api-nba-v1.p.rapidapi.com/players/statistics?season=2020&team=" + team,
+        headers={
+            'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+            'x-rapidapi-key': "1e5e5821femsh450b4f3086376a6p114414jsne8cc7f313f90"
+        }
+    )
+    status_code = response.status_code
+
+    games_counter = 0
+    fgp_per_game = []
+    tpp_per_game = []
+    points_per_game = []
+    assists_per_game = []
+    rebounds_per_game = []
+    blocks_per_game = []
+    steals_per_game = []
+    turnovers_per_game = []
+
+    if status_code == 200:
+        json = response.json()
+        for dict in json['response']:
+            try:
+                if (dict['player']['firstname'] == firstname and dict['player']['lastname']) == lastname:
+                    games_counter += 1
+                    fgp_per_game.append(float(dict['fgp']))
+                    tpp_per_game.append(float(dict['tpp']))
+                    points_per_game.append(dict['points'])
+                    assists_per_game.append(dict['assists'])
+                    rebounds_per_game.append(dict['totReb'])
+                    steals_per_game.append(dict['steals'])
+                    blocks_per_game.append(dict['blocks'])
+                    turnovers_per_game.append(dict['turnovers'])
+            except TypeError:
+                continue
+
+        if games_counter == 0:
+            print('We could not find your player, please try another one.')
+
+        else:
+            stats = {'name': f'{firstname} {lastname}',
+                     'fg%': round(statistics.mean(fgp_per_game), 2),
+                     '3p%': round(statistics.mean(tpp_per_game), 2),
+                     'points': round(statistics.mean(points_per_game)),
+                     'assists': round(statistics.mean(assists_per_game)),
+                     'rebounds': round(statistics.mean(rebounds_per_game)),
+                     'steals': round(statistics.mean(steals_per_game)),
+                     'blocks': round(statistics.mean(blocks_per_game)),
+                     'turnovers': round(statistics.mean(turnovers_per_game))
+                     }
+
+            return stats
+
+
+def print_stats(player):
+    for k, v in player.items():
+        if k == 'name':
+            print(f'\n{v.upper()}')
+        elif k == 'fg%':
+            print(f'\tField goal %: {v}')
+        elif k == '3p%':
+            print(f'\tThree point %: {v}')
+        else:
+            print(f'\t{k.title()}: {v}')
+
+
+def teams_list():
+    import requests
+    response = requests.get(
+        "https://api-nba-v1.p.rapidapi.com/teams?",
+        headers={
+            'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+            'x-rapidapi-key': "1e5e5821femsh450b4f3086376a6p114414jsne8cc7f313f90"
+        }
+    )
+    status_code = response.status_code
+    teams = []
+    if status_code == 200:
+        json = response.json()
+        for team in json['response']:
+            if team['nbaFranchise']:
+                teams.append(str(team['id']))
+            else:
+                continue
+        return teams
+    else:
+        return 'Error'
--- main.py
+++ main.py
@@ -1,24 +1,41 @@
-import numpy as np
 import requests
 
-from teams import welcome, team_finder
+from functions import welcome, team_finder, player_stats, print_stats
 
 while True:
     welcome()
     team = str(team_finder())
 
-    searched_players = []
-    searched_players_unique = []
+    # defino lista vacia para appendear diccionarios con las stats de los jugadores que cumplen los filtros ------------
+    searched_players_stats = []
 
-    # buscar usando atributos --------------------------------------------------------------------------------------
-
+    # attributes --------------------------------------------------------------------------------------
     print('\nInsert your player attributes:')
     position = input('\tPosition (B/SG/SF/PF/C):')
-    # age = int(input('\tAge:'))  # hacer la diferencia entre todays date y dict['birth']['date'], todavia no lo pude resolver
-    # years_pro = int(input('\tPro years:'))  # dict['nba']['pro']
-    height = float(input('\tHeight in meters (ex: 1.9):'))
-    weight = float(input('\tWeight in kilograms (ex: 110.5):'))
+    # print('\tAge')
+    # min_age = int(input('\t\tMinimum age:'))
+    # max_age = int(input('\t\tMaximum age:'))
+    print('\tHeight in meters (ex 1.9)')
+    min_height = float(input('\t\tMinimum height:'))
+    max_height = float(input('\t\tMaximum height:'))
+    print('\tWeight in kilograms (ex 110.5)')
+    min_weight = float(input('\t\tMinimum weight:'))
+    max_weight = float(input('\t\tMaximum weight:'))
 
+    # stats ----------------------------------------
+    print('\nInsert your player stats:')
+    min_fgp = float(input('\t\tMinimum field goal % (ex 30.5):'))
+    min_tpp = float(input('\t\tMinimum three point % (ex 30.5):'))
+    min_points = int(input('\t\tMinimum points:'))
+    min_rebounds = int(input('\t\tMinimum rebounds:'))
+    min_assists = int(input('\t\tMinimum assists:'))
+    min_steals = int(input('\t\tMinimum steals:'))
+    min_turnovers = int(input('\t\tMaximum turnovers:'))
+    min_blocks = int(input('\t\tMinimum blocks:'))
+
+    # buscamos los jugadores que cumplan con los filtros de atributos y los appendeamos a una lista de diccionarios
+    # calculando para cada uno el promedio por temporada de sus estadísticas usando la funcion player_stats
+    # esto se hace sobre el endpoint de players que trae datos del jugador, no estadísticas
     response = requests.get(
         "https://api-nba-v1.p.rapidapi.com/players?season=2020&team=" + team,
         headers={
@@ -30,77 +47,37 @@ while True:
 
     if status_code == 200:
         json = response.json()
-        # response es una key del json, su value es una lista de diccionarios
-        for dict in json['response']:
+        for player_dict in json['response']:
             try:
-                if float(dict['height']['meters']) >= height \
-                        and float(dict['weight']['kilograms']) >= weight:
-                    # and (today - dict['birth']['age']) >= age \
-
-                    searched_players.append(dict['firstname'] + ' ' + dict['lastname'])
-            except TypeError:
-                continue
-
-        if not searched_players:
-            print('\nWe are sorry, no players match your request.')
-        else:
-            print('\nThese players match your request:')
-            for player in np.unique(searched_players):
-                searched_players_unique.append(player)
-
-    else:
-        print('Error')
-
-    # stats ----------------------------------------
-
-    print('\nInsert your player stats:')
-    fgp = float(input('\tField goal percentage(ex 30.5):'))
-    tpp = float(input('\tThree point percentage(ex 3.5):'))
-    points = int(input('\tPoints:'))
-    rebounds = int(input('\tRebounds:'))
-    assists = int(input('\tAssists:'))
-    steals = int(input('\tSteals:'))
-    turnovers = int(input('\tTurnovers:'))
-    blocks = int(input('\tBlocks:'))
-    plus_minus = int(input('\tPlus/Minus +/-:'))
-
-    response = requests.get(
-        "https://api-nba-v1.p.rapidapi.com/players/statistics?season=2020&team=" + team,
-        headers={
-            'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
-            'x-rapidapi-key': "1e5e5821femsh450b4f3086376a6p114414jsne8cc7f313f90"
-        }
-    )
-    status_code = response.status_code
-
-    if status_code == 200:
-        json = response.json()
-        for dict in json['response']:
-            try:
-                if (dict['player']['firstname'] + ' ' + dict['player']['lastname']) in searched_players_unique \
-                        and dict['pos'] == position \
-                        and float(dict['fgp']) >= fgp \
-                        and float(dict['tpp']) >= tpp \
-                        and dict['points'] >= points \
-                        and dict['assists'] >= assists \
-                        and dict['totReb'] >= rebounds \
-                        and dict['steals'] >= steals \
-                        and dict['turnovers'] >= turnovers \
-                        and dict['blocks'] >= blocks \
-                        and float(dict['plusMinus']) >= plus_minus:
-                    searched_players.append(dict['player']['firstname'] + ' ' + dict['player']['lastname'])
+                if min_weight <= float(player_dict['weight']['kilograms']) <= max_weight \
+                        and min_height <= float(player_dict['height']['meters']) <= max_height:
+                    firstname = player_dict['firstname']
+                    lastname = player_dict['lastname']
+                    # llamo la funcion que usa el endpoint de players/statistics y devuelve un diccionario con los
+                    # promedios del jugador
+                    searched_players_stats.append(player_stats(team, firstname, lastname))
 
             except TypeError:
                 continue
 
-        if not searched_players:
+        if not searched_players_stats:
             print('\nWe are sorry, no players match your request.')
-        else:
-            print('\nThese players match your request:')
+            break
 
-            for player in np.unique(searched_players):
-                print(f'\t{player}')
-
-        # response es una key del json, su value es una lista de diccionarios
     else:
         print('Error')
+
+    # de la lista de diccionarios que creamos buscamos cada estadistica de cada jugador y la igualamos a los filtros
+    # que ingreso el usuario para printear solo aquellos que cumplan con dichas condiciones
+
+    for player in searched_players_stats:
+        if player['fg%'] >= min_fgp \
+                and player['3p%'] >= min_tpp \
+                and player['points'] >= min_points \
+                and player['assists'] >= min_assists \
+                and player['rebounds'] >= min_rebounds \
+                and player['steals'] >= min_steals \
+                and player['turnovers'] <= min_turnovers \
+                and player['blocks'] >= min_blocks:
+            print('\nThese players match your request:')
+            print(print_stats(player))
